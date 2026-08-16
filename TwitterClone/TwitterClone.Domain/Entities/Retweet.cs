f7e66d4a088cf83661a35105a120d3480674988b
asifolindo <olindoasif@gmail.com>
2026-08-16 09:24:47 +0600

    Encapsulation class assignment done.

---
+++ TwitterClone/TwitterClone.Domain/Entities/Retweet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class Retweet
+    {
+        private Guid _id;
+        private Guid _userId;
+        private Guid _tweetId;
+        private string _content;
+        private DateTime _createdAt;
+        private DateTime _modifiedAt;
+
+
+        public Retweet()
+        {
+            _id = Guid.NewGuid();
+            _createdAt = DateTime.UtcNow;
+        }
+
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+
+        public Guid UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+            set { _tweetId = value; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+        }
+
+        public DateTime ModifedAt
+        {
+            get { return _modifiedAt; }
+            set { _modifiedAt = value; }
+        }
+    }
+}
